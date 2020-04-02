9eebe759ccfa32d337cbaa932fe87ce97d5471eb
刘少腾 <liushaoteng@MacBook-Pro.local>
2020-04-02 10:55:21 -0700

    update

--- moco/builder.py
+++ moco/builder.py
@@ -8,7 +8,7 @@ class MoCo(nn.Module):
     Build a MoCo model with: a query encoder, a key encoder, and a queue
     https://arxiv.org/abs/1911.05722
     """
-    def __init__(self, base_encoder, dim=128, K=65536, m=0.999, T=0.07, mlp=False):
+    def __init__(self, base_encoder, dim=128, K=65536, m=0.999, T=0.07, mlp=False, width=-1):
         """
         dim: feature dimension (default: 128)
         K: queue size; number of negative keys (default: 65536)
@@ -23,8 +23,12 @@ class MoCo(nn.Module):
 
         # create the encoders
         # num_classes is the output fc dimension
-        self.encoder_q = base_encoder(num_classes=dim)
-        self.encoder_k = base_encoder(num_classes=dim)
+        if width != -1: # stliu: which means this is a resnet_ttt
+            self.encoder_q = base_encoder(num_classes=dim, width=width)
+            self.encoder_k = base_encoder(num_classes=dim, width=width)
+        else:
+            self.encoder_q = base_encoder(num_classes=dim)
+            self.encoder_k = base_encoder(num_classes=dim)
 
         if mlp:  # hack: brute-force replacement
             dim_mlp = self.encoder_q.fc.weight.shape[1]
--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 python main_moco.py \
-  -a resnet50 \
+  -a resnet_ttt \
   --lr 0.03 \
   --batch-size 256 \
-  --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
-  [../data/myCIFAR-10-C/]
+  --dist-url 'tcp://localhost:10003' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --results ./results \
+  --width 8 \
+  ../data/myCIFAR-10-C/
