7172c00ac51a6a7cc048b5007e51bfa5013c2ca4
刘少腾 <liushaoteng@MacBook-Pro.local>
2020-04-02 17:17:53 -0700

    add SVM

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@ python main_moco.py \
   -a resnet_ttt \
   --lr 0.03 \
   --batch-size 256 \
-  --dist-url 'tcp://localhost:10003' --multiprocessing-distributed --world-size 1 --rank 0 \
-  --results ./results \
-  --width 8 \
+  --dist-url 'tcp://localhost:10004' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --model_path ./results/model \
+  --tb_path ./results/tb \
+  --width 1 \
   ../data/myCIFAR-10-C/
